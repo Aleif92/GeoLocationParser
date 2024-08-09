c778eca4afedad26fe9bec9366a373285c3f71e0
Alex Leifermann <aleif92@outlook.com>
2024-08-08 19:07:56 -0500

    Fixed for loop. Final Salution

--- GeoLocationParser/Program.cs
+++ GeoLocationParser/Program.cs
@@ -3,56 +3,84 @@ using System;
 using System.Linq;
 using System.IO;
 using GeoCoordinatePortable;
-namespace GeoLocationParser;
 
-class Program
+using GeoCoordinatePortable;
+
+namespace GeoLocationParser
 {
-    private static readonly ILog logger = new TacoLogger();
-    const string CsvPath = "TacoBell-US-AL.csv";
-    static void Main(string[] args)
+    class Program
     {
-        logger.LogInfo("Log Initialized");
+        private static readonly ILog logger = new TacoLogger();
+        const string CsvPath = "TacoBell-US-AL.csv";
 
-
-
-        var lines = File.ReadAllLines(CsvPath);
-
-        if (lines.Length == 0)
+        static void Main(string[] args)
         {
-            logger.LogError("File has no input");
+            logger.LogInfo("Log Initialized");
+
+
+
+            var lines = File.ReadAllLines(CsvPath);
+
+            if (lines.Length == 0)
+            {
+                logger.LogError("File has no input");
+            }
+
+            if (lines.Length == 1)
+            {
+                logger.LogWarning("File only has one line of input.");
+            }
+
+            logger.LogInfo($"Lines: {lines[0]}");
+
+            var parser = new TacoParser();
+            var locations = lines.Select(parser.Parse).ToArray();
+
+            ITrackable tacobell1 = null;
+            ITrackable tacobell2 = null;
+
+            double distance = 0;
+
+            for (int i = 0; i < locations.Length; i++)
+            {
+                var locA = locations[i];
+                var corA = new GeoCoordinate();
+                corA.Latitude = locA.Location.Latitude;
+                corA.Longitude = locA.Location.Longitude;
+
+                for (int j = 0; j < locations.Length; j++)
+                {
+                    var locB = locations[j];
+                    var corB = new GeoCoordinate();
+                    corB.Latitude = locB.Location.Latitude;
+                    corB.Longitude = locB.Location.Longitude;
+                    if (corA.GetDistanceTo(corB) > distance)
+                    {
+                        distance = corA.GetDistanceTo(corB);
+                        tacobell1 = locA;
+                        tacobell2 = locB;
+
+                    }
+
+                }
+
+                
+
+
+
+
+
+
+
+
+            }
+            Console.WriteLine(
+                $"{tacobell1.Name} and {tacobell2.Name} are the two Tacobell locations that are farthest apart.\n That is about {Math.Round(distance)} meters apart. That is exactly {Math.Round(distance) * 0.00062137} miles.");
+
+
+
+
+
         }
-
-        if (lines.Length == 1)
-        {
-            logger.LogWarning("File only has one line of input.");
-        }
-        
-        logger.LogInfo($"Lines: {lines[0]}");
-
-        var parser = new TacoParser();
-        var locations = lines.Select(parser.Parse).ToArray();
-
-        ITrackable tacobell1 = null;
-        ITrackable tacobell2 = null;
-
-        double distance = 0;
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-            var locA = new GeoCoordinate();
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
--- GeoLocationParser/obj/Debug/net8.0/GeoLocationParser.AssemblyInfo.cs
+++ GeoLocationParser/obj/Debug/net8.0/GeoLocationParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeoLocationParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df026d812aca547cd345face9ed318c03328550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc00ee5e369007f0cfa2dcdbe03e0c607c0f72c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeoLocationParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeoLocationParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
